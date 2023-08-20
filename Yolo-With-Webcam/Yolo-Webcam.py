3206600f697ce44ab432064ba0d16d03bd7d7893
Yogesh Vashisth <130190342+yogeshvas@users.noreply.github.com>
2023-08-21 01:00:44 +0530

    Update Yolo-Webcam.py

--- Yolo-With-Webcam/Yolo-Webcam.py
+++ Yolo-With-Webcam/Yolo-Webcam.py
@@ -4,10 +4,10 @@ import cvzone
 import math
 import time
 
-# cap = cv2.VideoCapture(0)  # For Webcam
-# cap.set(3, 1280)
-# cap.set(4, 720)
-cap = cv2.VideoCapture("../Videos/cars.mp4")  # For Video
+cap = cv2.VideoCapture(0)  # For Webcam
+cap.set(3, 1280)
+cap.set(4, 720)
+
 
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
